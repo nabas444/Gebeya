c1cca79de2d82b9685c38fddbb53d66af1b4c7c2
nabas444 <natnaelabebe848@gmail.com>
2026-02-27 22:07:54 +0300

    feat(cart): Implement cart subtotal calculation and checkout navigation
    - Add total price calculation using basket items
    - Import CurrencyFormat component for price formatting
    - Display subtotal with item count and calculated total
    - Add gift checkbox option
    - Implement continue to checkout link with payment route
    - Clean up code formatting and imports

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 import Layout from "../../components/Layout/Layout";
-import classes from "./Cart.module.css";  
+import classes from "./Cart.module.css";
 import { DataContext } from "../../components/DataProvider/DataProvider";
 import { useContext } from "react";
-import ProductCard from '../../components/product/ProductCard';
+import ProductCard from "../../components/product/ProductCard";
+import CurrencyFormat from "../../components/CurrencyFormat/CurrencyFormat";
+import { Link } from "react-router-dom";
 function Cart() {
-  const [{ basket }] = useContext(DataContext);
+  const [{ basket, user }, dispatch] = useContext(DataContext);
+  const total = basket.reduce((amount, item) => {
+    return item.price + amount;
+  }, 0);
+
   return (
     <Layout>
       <section className={classes.cartSection}>
@@ -14,35 +20,41 @@ function Cart() {
 
           <h3>Your Cart is Empty</h3>
           <hr />
-          {
-            basket?.length==0 ? (<p>You have no items in your cart. <br /> To buy one or more items, click "Add to Cart" next to the item.</p>) : (
-              basket?.map((item,i) => {
-              return <ProductCard
-              key={i}
-               product={item} 
-               renderDesc={true} 
-               renderAdd={false}
-               flex={true}
-               />
-              }
-            )
-          )} 
-        
+          {basket?.length == 0 ? (
+            <p>
+              You have no items in your cart. <br /> To buy one or more items,
+              click "Add to Cart" next to the item.
+            </p>
+          ) : (
+            basket?.map((item, i) => {
+              return (
+                <ProductCard
+                  key={i}
+                  product={item}
+                  renderDesc={true}
+                  renderAdd={false}
+                  flex={true}
+                />
+              );
+            })
+          )}
         </div>
-        {
-          basket?.length !==0&&(
+        {basket?.length !== 0 && (
+          <div>
             <div>
-<div>
-  <p>Subtotal ({basket?.length} items):</p>
-</div>
-              </div>
-          
-        
-       ) }
-        
+              <p>Subtotal ({basket?.length} items)</p>
+              <CurrencyFormat amount={total} />
+            </div>
+            <span>
+              <input type="checkbox" />
+              <small>This order contains a gift</small>
+            </span>
+            <Link to="payments">Continue to checkout</Link>
+          </div>
+        )}
       </section>
     </Layout>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
