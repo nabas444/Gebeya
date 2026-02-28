cab7042667ff22a610791bada961c0f4f5aa4296
nabas444 <natnaelabebe848@gmail.com>
2026-02-28 04:11:23 +0300

    chore: install firebase sdk
    Added firebase dependency to package.json and updated package-lock.json.
    This will be used for authentication and database management in the
    next phase of development.

---
+++ src/utility/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqAXYey5prbl5_UHLHtQ6vzLDGGGn4ZiU",
+  authDomain: "gebeyacom.firebaseapp.com",
+  projectId: "gebeyacom",
+  storageBucket: "gebeyacom.firebasestorage.app",
+  messagingSenderId: "67117720272",
+  appId: "1:67117720272:web:f45a8e9cf91cd5c3d9b4d5"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = app.getAuth(app);
