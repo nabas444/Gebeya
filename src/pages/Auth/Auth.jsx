539da907a209bd88aa0b07bcf036f4979ab5aafe
nabas444 <natnaelabebe848@gmail.com>
2026-03-01 21:17:04 +0300

    refactor: replace Signup page with unified Auth component
    - Renamed Signup to Auth
    - Updated routing to use Auth component
    - Renamed Signup.module.css to Auth.module.css
    - Removed old Signup.jsx file
    - Initialized Auth page structure with Layout wrapper

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import "./Auth.module.css";
+function Auth() {
+  return (
+    <Layout>
+      <section className="{classes.">
+        <div>
+          <img src="" alt="gebeyacom logo" />
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default Auth;
